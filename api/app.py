1aa06261b4ae536473024e09564c49aab5d499af
ulisestc <ulisestc27@gmail.com>
2026-04-13 13:41:28 -0600

    get /api/dataset

--- api/app.py
+++ api/app.py
@@ -1,16 +1,39 @@
-from fastapi import FastAPI
+from contextlib import asynccontextmanager # lifespan event
+
+from fastapi import FastAPI, Request
 from pydantic import BaseModel, Field
+
+import pandas as pd
+
+#lifespan custom para cargar el dataset
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    #startup
+    print("Iniciando servidor, cargando data set")
+    app.state.df = pd.read_csv("../data/processed/clean_food_delivery.csv")
+    app.state.df = app.state.df.replace({float('nan'): None}) #evitar problemas con NaN en JSON
+    #initialize resources
+    yield
+    #shutdown
+    print("Apagando servidor")
+
 
 class Estrategia(BaseModel):
     dia_semana: int = Field(ge=0, le=6)
     hora_inicio: int = Field(ge=0, le=23)
     porcentaje_descuento: int = Field(gt=0, le=100)
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/api/")
 def read_root():
     return {"Hello": "World"}
+
+# Endpoint para obtener el dataset (solo una muestra)
+@app.get("/api/dataset")
+def obtener_dataset(request: Request):
+    df = request.app.state.df
+    return df.head().to_dict(orient="records")
 
 @app.get("/api/analisis-retrasos")
 def analizar_retrasos():
